5d947f9a0ccc6b38523a60fb3d9a543ba0c10b64
rahuldahal <rdaahal@gmail.com>
2024-01-11 20:21:10 +0545

    chore: prisma module and service config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { MyConfigModule } from './config.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [MyConfigModule],
+  imports: [MyConfigModule, AuthModule, PrismaModule],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  signUp() {
+    return {
+      message: 'signUp successful!',
+    };
+  }
+
+  signIn() {
+    return {
+      message: 'signIn successful!',
+    };
+  }
+}
