34c7b0b91aed2fcadb42c7304cf6c2e9243b135e
rahuldahal <rdaahal@gmail.com>
2024-05-21 08:46:50 +0545

    refactor: separate authDTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { AuthDto } from './dto';
+import { signInDto, signUpDto } from './dto';
 import { AuthService } from './auth.service';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
@@ -10,18 +10,18 @@ export class AuthController {
 
   @Post('signup')
   @ApiOperation({ summary: 'Sign up a new user' })
-  @ApiBody({ type: AuthDto, description: 'User registration information' })
+  @ApiBody({ type: signUpDto, description: 'User registration information' })
   @HttpCode(201)
   // DTO: Data Transfer Object more - https://docs.nestjs.com/techniques/validation
-  signUp(@Body() dto: AuthDto) {
+  signUp(@Body() dto: signUpDto) {
     return this.authService.signUp(dto);
   }
 
   @Post('signin')
   @ApiOperation({ summary: 'Sign in an existing user' })
-  @ApiBody({ type: AuthDto, description: 'User credentials for sign-in' })
+  @ApiBody({ type: signInDto, description: 'User credentials for sign-in' })
   @HttpCode(200)
-  signIn(@Body() dto: AuthDto) {
+  signIn(@Body() dto: signInDto) {
     return this.authService.signIn(dto);
   }
 }
