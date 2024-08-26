050f000f6a58b69571b5380a02da2f15acb2ec4f
rahuldahal <rdaahal@gmail.com>
2024-08-26 21:49:55 +0545

    chore: update project name

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -3,9 +3,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export default function setupSwagger(app: INestApplication) {
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('Co-edit API Documentation')
+    .setTitle('Syncode API Documentation')
     .setDescription(
-      'Use the documentation of the API below as a reference to make requests.'
+      'Use the documentation of the API below as a reference to make requests.',
     )
     .setVersion('1.0')
     .addBearerAuth()
